24dfbd25525fe3eec18abdc183dbfbcb4b160591
Usman Shaikh <32120864+NobleUsman@users.noreply.github.com>
2021-09-20 21:42:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get("/", function(req, res) {
 
   //3) ---for requesting other server, we either use node's native "HTTPS" module, or "AXIOS" etc.
   // [DONT FORGET to add https:// before URL] .... why .... because here we are in a server and not in a browser, server cannot do that
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Mumbai&units=metric&appid=b40395dce37022e256d3be7ec2a30480"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Mumbai&units=metric&appid="
 
   https.get(url, function (response) {
     console.log(response.statusCode);
